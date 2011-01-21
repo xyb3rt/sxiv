72e56e520704522c52abd349566b1523ad96c808
Bert <ber.t@gmx.com>
2011-01-21 12:58:02 +0100

    Made config.h a bit clearer

--- config.h
+++ config.h
@@ -1,16 +1,16 @@
 /* default window dimensions:                            */
-#define WIN_WIDTH  800
-#define WIN_HEIGHT 600
+#define WIN_WIDTH   800
+#define WIN_HEIGHT  600
 
 /* default color to use for window background:           *
  *   (see X(7) "COLOR NAMES" section for valid values)   */
-#define BG_COLOR   "#888888"
+#define BG_COLOR    "#888888"
 
-/* default scale mode when loading a new image:          *
+/* how should images be scaled when they are loaded?:    *
  *   SCALE_DOWN: 100%, but fit large images into window, *
  *   SCALE_FIT:  fit all images into window,             *
  *   SCALE_ZOOM: use current zoom level, 100% at startup */
-#define SCALE_MODE SCALE_DOWN
+#define SCALE_MODE  SCALE_DOWN
 
 /* levels (percent) to use when zooming via '-' and '+': */
 static const float zoom_levels[] = {
