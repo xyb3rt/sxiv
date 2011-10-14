36177fb180d0a3e478bdee86664285c6a21c0be8
Bert Münnich <ber.t@gmx.com>
2011-10-14 10:40:49 +0200

    Updated contact information

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /* sxiv: main.c
- * Copyright (c) 2011 Bert Muennich <muennich at informatik.hu-berlin.de>
+ * Copyright (c) 2011 Bert Muennich <be.muennich at googlemail.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
