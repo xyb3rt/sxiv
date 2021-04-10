819b2624af0b7210296733925fd0136358c8dd75
Naheel-Azawy <naheelazawy@gmail.com>
2021-04-10 03:43:23 +0300

    breaking change: bar is hidden by default. -b shows the bar

--- options.c
+++ options.c
@@ -64,7 +64,7 @@ void parse_options(int argc, char **argv)
 
 	_options.fullscreen = false;
 	_options.embed = 0;
-	_options.hide_bar = false;
+	_options.hide_bar = true;
 	_options.geometry = NULL;
 	_options.res_name = NULL;
 
@@ -89,7 +89,7 @@ void parse_options(int argc, char **argv)
 				_options.animate = true;
 				break;
 			case 'b':
-				_options.hide_bar = true;
+				_options.hide_bar = false;
 				break;
 			case 'c':
 				_options.clean_cache = true;
